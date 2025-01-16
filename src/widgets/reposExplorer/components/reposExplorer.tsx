b1b1a6480902e9ec83943fe778d9a2dca230cffb
Табаков Александр <Aleksandr.Tabakov@gorparkovki.ru>
2025-01-16 19:39:07 +0300

    first variant

--- src/widgets/reposExplorer/components/reposExplorer.tsx
+++ src/widgets/reposExplorer/components/reposExplorer.tsx
@@ -3,19 +3,60 @@ import { RepoCard } from '@/entities/repositories';
 import { useRepositoriesByOwnerQuery } from '@/entities/repositories/gql/queries/repositoriesByOwner.graphql';
 import { Input } from '@/shared/components/ui/Input';
 import { Spinner } from '@/shared/components/ui/Spinner';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import { InView } from 'react-intersection-observer';
 
 export const ReposExplorer = () => {
   const [login, setLogin] = useState('');
 
-  const { data, loading: isLoading } = useRepositoriesByOwnerQuery({
+  const {
+    data,
+    loading: isLoading,
+    fetchMore,
+  } = useRepositoriesByOwnerQuery({
     variables: {
       login,
+      cursor: null,
     },
     notifyOnNetworkStatusChange: true,
     skip: !login,
   });
+
+  const hasNextPage = data?.repositoryOwner?.repositories.pageInfo.hasNextPage;
+  const endCursor =
+    data?.repositoryOwner?.repositories.pageInfo.endCursor || null;
   const repos = data?.repositoryOwner?.repositories.nodes;
+
+  const loadMoreRepos = useCallback(() => {
+    fetchMore({
+      variables: {
+        cursor: endCursor,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEntries =
+          fetchMoreResult.repositoryOwner?.repositories.nodes || [];
+        const endCursor =
+          fetchMoreResult.repositoryOwner?.repositories.pageInfo.endCursor;
+        const hasNextPage =
+          fetchMoreResult.repositoryOwner?.repositories.pageInfo.hasNextPage ??
+          false;
+        return {
+          repositoryOwner: {
+            repositories: {
+              pageInfo: {
+                endCursor,
+                hasNextPage,
+              },
+              nodes: [
+                ...(previousResult.repositoryOwner?.repositories.nodes || []),
+                ...newEntries,
+              ],
+            },
+          },
+        };
+      },
+    });
+  }, [endCursor, fetchMore]);
 
   return (
     <div className="flex flex-col gap-8 w-full max-w-prose">
@@ -44,6 +85,15 @@ export const ReposExplorer = () => {
       )}
       {!!login && !isLoading && !repos?.length && <p>Репозитории не найдены</p>}
       {isLoading && <Spinner className="self-center" />}
+      {hasNextPage && login && !isLoading && (
+        <InView
+          onChange={inView => {
+            if (inView) {
+              loadMoreRepos();
+            }
+          }}
+        />
+      )}
     </div>
   );
 };
